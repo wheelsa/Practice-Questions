17f55fe8eecf52b34da4a10d48fc3e2a3a340402
Seth Wheeler <wheeler.seth@gmail.com>
2020-05-16 17:59:19 -0600

    added palindrome check and reverse int check

--- src/com/wheelsa/Main.java
+++ src/com/wheelsa/Main.java
@@ -3,24 +3,55 @@ package com.wheelsa;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        reverseString("Yolo");
+
+//        reverseString("Yolo");
+//    System.out.println(palindrome("abba"));
+        System.out.println(reverseInteger(1234));
     }
 
 
-public static String reverseString(String str){
-    if(str.length() <= 1){
-        return str;
-    }
-    String reversedStr = "";
+    //reverse an integer
 
-    for(int i = str.length() - 1; i >= 0; i-- ){
+   public static int reverseInteger(int number){
+        int reversedNumber = 0;
 
-        reversedStr = reversedStr + str.charAt(i);
+        while (number != 0){
+            int digit = number % 10;
+            reversedNumber = (reversedNumber * 10) + digit;
+            number /= 10;
+        }
+        return reversedNumber;
+   }
 
-    }
-    System.out.println(reversedStr);
-    return reversedStr;
-}
+
+//Function tests if string is palindrome
+//    public static boolean palindrome(String str){
+//        int j = str.length() - 1;
+//        int i = 0;
+//        while( i < j){
+//            if(str.charAt(i) != str.charAt(j)){
+//                return false;
+//            }
+//            i++;
+//            j--;
+//        }
+//        return true;
+//    }
+
+//
+//public static String reverseString(String str){
+//    if(str.length() <= 1){
+//        return str;
+//    }
+//    String reversedStr = "";
+//
+//    for(int i = str.length() - 1; i >= 0; i-- ){
+//
+//        reversedStr = reversedStr + str.charAt(i);
+//
+//    }
+//    System.out.println(reversedStr);
+//    return reversedStr;
+//}
 
 }

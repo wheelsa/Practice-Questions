6075a7db0fdd0b26539890dd134a7450d2a038d6
Seth Wheeler <wheeler.seth@gmail.com>
2020-05-16 18:47:23 -0600

    fizzBuzz

--- src/com/wheelsa/Main.java
+++ src/com/wheelsa/Main.java
@@ -1,16 +1,55 @@
 package com.wheelsa;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
 //        reverseString("Yolo");
 //    System.out.println(palindrome("abba"));
-        System.out.println(reverseInteger(1234));
+//        System.out.println(reverseInteger(1234));
+//        System.out.println(maxOccuringCharString("hello lula loo"));
+        fizzBuzz(25);
     }
 
 
     //reverse an integer
+
+    public static void fizzBuzz(int n){
+        int i = 0;
+        while(i++ != n){
+            if( i % 3 == 0 && i% 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0){
+                System.out.println("Fizz");
+            }else if ( i % 5 == 0){
+                System.out.println("Buzz");
+            }else {
+                System.out.println(i);
+            }
+            ;
+        }
+    }
+
+    public static char maxOccuringCharString(String str){
+//        final int ASCII_SIZE = 256;
+        int[] hashCount = new int[256];
+        for(int i = 0; i < str.length(); i++) {
+            hashCount[str.charAt(i)]++;
+        }
+        System.out.println(Arrays.toString(hashCount));
+        int max = -1;
+        char result = ' ';
+
+        for(int i = 0; i< str.length(); i++){
+            if(max < hashCount[str.charAt(i)]){
+                max = hashCount[str.charAt(i)];
+                result = str.charAt(i);
+            }
+        }
+        return result;
+    }
 
    public static int reverseInteger(int number){
         int reversedNumber = 0;
